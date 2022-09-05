2a845b5ab67da2e40743d464eb10f4c07f0eec08
Palakgarg5 <65445239+Palakgarg5@users.noreply.github.com>
2022-09-05 20:32:07 +0530

    Create Average3Numbers.cpp

---
+++ BasicPrograms/Average3Numbers.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+  int a, b, c, avg;
+  cout<<"Enter the value of a , b and c : ";
+  cin >> a >> b >> c;
+  avg = (a + b + c)/3;
+  cout<<"The average is : "<< avg;
+}
