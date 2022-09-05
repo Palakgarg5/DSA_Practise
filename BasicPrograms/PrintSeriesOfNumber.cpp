f6ba8bf0597438f98810e15e5bca2fcd9214e89c
Palakgarg5 <65445239+Palakgarg5@users.noreply.github.com>
2022-09-05 21:10:43 +0530

    Create PrintSeriesOfNumber.cpp

---
+++ BasicPrograms/PrintSeriesOfNumber.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+ int n, num=1;
+  cout<<"Enter the number : ";
+  cin>>n;
+  while(num<=n)
+  {
+    cout<<num<<endl;
+    num = num +1;
+  }
+  
+}
