8eb29224019bf5ad6429fe922f2abc368ef47a7a
Palakgarg5 <65445239+Palakgarg5@users.noreply.github.com>
2022-09-05 21:25:25 +0530

    Create SeriesOfOddNum.cpp

---
+++ BasicPrograms/SeriesOfOddNum.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+ int n, num=1;
+  cout<<"Enter the value of n : ";
+  cin>> n ;
+  cout<<"Series of Odd numbers : "<<endl;
+  while(num<=n)
+    {
+      cout<<num<<endl;;
+      num =num + 2;
+    
+    }
+}
