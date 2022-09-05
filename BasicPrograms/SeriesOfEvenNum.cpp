1e8c7437a94ca37cf872fbf0c403f4b9285ee667
Palakgarg5 <65445239+Palakgarg5@users.noreply.github.com>
2022-09-05 21:21:11 +0530

    Create SeriesOfEvenNum.cpp

---
+++ BasicPrograms/SeriesOfEvenNum.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+ int n ,num=2;
+  cout<<"Enter the number : ";
+  cin>>n;
+  cout<<"The series of Even numbers : "<<endl;
+  while(num<=n)
+    {
+      cout<<num<<endl;
+      num =num +2;
+    }
+}
